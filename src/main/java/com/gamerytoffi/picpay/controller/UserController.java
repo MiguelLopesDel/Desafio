36b487a6bc80bd56e813cdef777902db0aa8bd39
MiguelLopesDel <miguellopesdelmondes2@gmail.com>
2024-03-17 13:08:33 -0300

    small mudances

--- src/main/java/com/gamerytoffi/picpay/controller/UserController.java
+++ src/main/java/com/gamerytoffi/picpay/controller/UserController.java
@@ -3,7 +3,6 @@ package com.gamerytoffi.picpay.controller;
 import com.gamerytoffi.picpay.domain.User;
 import com.gamerytoffi.picpay.domain.dto.UserDTO;
 import com.gamerytoffi.picpay.service.UserService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,19 +11,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@AllArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
         User user = userService.createUser(userDTO);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
-        return new ResponseEntity<>(this.userService.getAllUsers(), HttpStatus.OK);
+        return ResponseEntity.ok(this.userService.getAllUsers());
     }
 }
