016db7c78c1fe9dd561e7fd36c5c639aa21ce998
MiguelLopesDel <miguellopesdelmondes2@gmail.com>
2024-03-28 00:24:44 -0300

    auditoria ativada

--- src/main/java/com/gamerytoffi/picpay/PicpayApplication.java
+++ src/main/java/com/gamerytoffi/picpay/PicpayApplication.java
@@ -1,11 +1,21 @@
 package com.gamerytoffi.picpay;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PicpayApplication {
     public static void main(String[] args) {
         SpringApplication.run(PicpayApplication.class, args);
+    }
+
+    @Bean
+    NewTopic notificationTopic() {
+        return TopicBuilder.name("transaction_notification").build();
     }
 }
